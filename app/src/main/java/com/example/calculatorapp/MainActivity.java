98b2bba2af54e1496ed3cb037bb3b17ccfbdeab2
randomuser191 <88069151+randomuser191@users.noreply.github.com>
2023-09-11 12:55:14 -0500

    Finishing Touches/Foolproofing

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int numInp = 0;
     private String var1 = "";
     private String var2 = "";
     private String operation = "";
@@ -53,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (v.getId() == R.id.piButton) {
             resString = currText + "π";
         } else if (v.getId() == R.id.clearButton) {
-            resString = "";
+            resString = "0";
             Log.i("msg", "cleared");
         } else if (v.getId() == R.id.changeSign) {
             double d = Double.parseDouble(currText) * -1;
@@ -85,35 +84,36 @@ public class MainActivity extends AppCompatActivity {
         if(v.getId() == R.id.equalsButton){
             if(equation){
                 var2 = currText;
-                Log.i("msg", "Var1: " + var1 + ", Var 2: " + var2 + ", Opeartor: " + operation);
                 resString = equate(var1, var2, operation);
                 equation = false;
             }
         }
-        //set var 1 and var 2
-        //on equals press or different operator press return result
         resultView.setText(resString);
     }
     public String equate (String v1, String v2, String operation){
+        reset = true;
         if(v1.equals("") || v2.equals("") || operation.equals("")) return v1;
         if(v1.contains("π")){
-            double num = Double.parseDouble(v1.replace("π", ""));
+            double num;
+            if(v1.length() < 2) num = 1;
+            else num = Double.parseDouble(v1.replace("π", ""));
             v1 = num * Math.PI + "";
         }
-        if(v2.equals("π")){
-            double num = Double.parseDouble(v2.replace("π", ""));
+        if(v2.contains("π")){
+            double num;
+            if(v2.length() < 2) num = 1;
+            else num = Double.parseDouble(v2.replace("π", ""));
             v2 = num * Math.PI + "";
         }
         double var1 = Double.parseDouble(v1);
         double var2 = Double.parseDouble(v2);
         double numResult = 0.0;
-        String result = "";
-        Log.i("msg", "Var1: " + var1 + ", Var 2: " + var2 + ", Opeartor: " + operation);
+        String result;
         if(operation.equals("multiply")){
             numResult = var1 * var2;
-            Log.i("msg", "Result: " + numResult);
         }
         else if(operation.equals("divide")){
+            if(var1 != 0 && var2 == 0) return "undefined";
             numResult = var1 / var2;
         }
         else if(operation.equals("add")){
@@ -124,12 +124,10 @@ public class MainActivity extends AppCompatActivity {
         }
         else if(operation.equals("modulo")){
             numResult = var1 % var2;
-            Log.i("msg", "DoubleRes: " + 6 % 5);
         }
         numResult = Math.floor(numResult * 100000) / 100000;
         if(numResult % 1 == 0){
             result = (int)(numResult) + "";
-            Log.i("msg", "Int: " + numResult);
         }else{
             result = numResult + "";
         }
